bf51f9bd8f075bcd3104e1cc23dc5c3bd7c7a154
kbbushman <k.bushman@yahoo.com>
2021-03-20 22:37:45 -0700

    Adds authRequired to addUser

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,4 +1,8 @@
+const { authRequired } = require('../utils');
+
 async function addUser(parent, args, context, info) {
+  authRequired(context);
+
   const newUser = await context.prisma.user.create({ data: { ...args.userInput } });
 
   return newUser;
